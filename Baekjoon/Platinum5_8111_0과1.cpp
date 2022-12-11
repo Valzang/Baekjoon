f30cfd8bbaa2a5c7dd7f09ea947278088ca74fc7
JCW_HOME <72681774+Valzang@users.noreply.github.com>
2022-12-11 22:56:06 +0900

    Update Platinum5_8111_0과1.cpp

--- Baekjoon/Platinum5_8111_0과1.cpp
+++ Baekjoon/Platinum5_8111_0과1.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -16,111 +17,87 @@ using namespace std;
 
 예를 들어, 101은 구사과가 좋아하는 수이다.
 
-첫째 줄에 테스트 케이스의 개수 T(T < 10)가 주어진다.
-둘째 줄부터 T개의 줄에는 자연수 N이 한 줄에 하나씩 주어진다. N은 20,000보다 작거나 같은 자연수이다.
+- 첫째 줄에 테스트 케이스의 개수 T(T < 10)가 주어진다.
+- 둘째 줄부터 T개의 줄에는 자연수 N이 한 줄에 하나씩 주어진다. N은 20,000보다 작거나 같은 자연수이다.
 
-각각의 테스트 케이스마다 N의 배수이면서, 구사과가 좋아하는 수를 아무거나 출력한다. 만약, 그러한 수가 없다면 BRAK을 출력한다.
+- 각각의 테스트 케이스마다 N의 배수이면서, 구사과가 좋아하는 수를 아무거나 출력한다.
+	만약, 그러한 수가 없다면 BRAK을 출력한다.
 
 */
 
-class DigitResult
+// string을 점점 키워나가면서 중간에 N으로 나눠떨어지면 출력. string의 크기가 100을 초과하면 BRAK 출력
+// (A+B)%C = ((A%C) + (B%C)) %C
+// (A*B)%C = ((A%C) * (B%C)) %C
+// (A*A)%C = ((A%C) * (A%C)) %C
+//	=> (10^99)%C = ((10^9 % C)^10)%C
+// ex) 123456 % N =>
+//		(1* 10^5 + 2*10^4 + 3*10^3 + 4*10^2 + 5*10^1 + 6*10^0) % N
+//		=> ((1* 10^5 % N) + (2*10^4 % N ) + ( 3*10^3 % N ) + (4*10^2 % N ) + (5*10^1 %N ) + (6*10^0 % N )) %N
+
+// int max => 2,147,483,647		총 10자리 ( 10^9)
+// (10^9)^9
+// 10^10 = 10^9 * 10
+// 10^99 = (10^9)^9
+
+int PowerModular(int num, int pow, int mod)
 {
-	vector<int> cal;
-
-public:
-	DigitResult()
+	int answer = 1;
+	while (pow)
 	{
-		cal.reserve(100);
-		fill(cal.begin(), cal.end(), 0);
+		if (pow % 2 == 1)
+			answer = (answer * num % mod);
+		num = num * num % mod;
+		pow /= 2;
 	}
+	return answer;
+}
 
-	DigitResult(int num)
-	{
-		cal.reserve(100);
-		fill(cal.begin(), cal.end(), 0);
-
-		string strNum = to_string(num);
-		for (int i = 0; i < strNum.size(); ++i)
-		{
-			cal[i] = num % 10;
-			num /= 10;
-		}
-	}
-
-	// 1의 자릿수가 0번부터 채워짐.
-
-	bool Calculate(vector<int> tempCal)
-	{
-		if (tempCal[tempCal.size() - 1] >= 10)
-			return false;
-
-		for (int i = 0; i < tempCal.size()-1; ++i)
-		{
-			string num = to_string(tempCal[i]);
-			while (num.size() != 1 || num != "0")
-			{
-				// 0이나 1 이외가 나왔을 시
-				if ((int)num.back() - 48 > 1)
-					return false;
-				// 가장 큰 자릿수의 수를 해당 위치로 옮겨줌.
-				tempCal[i + num.size() - 1] += (int)num.front() - 48;
-				num = to_string(tempCal[i] / 10);			
-			}			
-		}
-		if (tempCal[tempCal.size() - 1] < 10)
-		{
-			for (int i = tempCal.size() - 1; i >= 0; --i)
-			{
-				if (tempCal[i] != 0)
-					cout << tempCal[i];
-			}
-			cout << '\n';
-			return true;
-		}
-		return false;
-	}
-
-	bool Multiply(int val)
-	{
-		vector<int> tempCal = cal;
-		for (int i = 0; i < tempCal.size(); ++i)
-			tempCal[i] *= val;
-		
-		return Calculate(tempCal);
-	}
-};
 
 void Solution(int N)
 {
-	int result = -1;
-	DigitResult digitResult(N);
-	vector<int> divNum;
-	// 끝자리가 0이나 1이 나오도록 숫자 제한.
-	// divNum도 한 자리 한자리로 해야함.
-	for (int i = 1; i <= 9; ++i)
-	{
-		if ((N * i) % 10 <= 1)
-			divNum.push_back(i);
-	}
+	string stringN = to_string(N);
+	string initNum = "1";	
+	// N의 자릿수와 동일하게 맞춰주기
+	for (int i = 1; i < stringN.size(); ++i)
+		initNum.append("0");	
 
-	int curIdx = 0;
-	while (true)
+	int totalCount1 = 1;
+	int curIndex = 0;
+	int totalNumCount = initNum.size();
+
+	int mod = 0;
+	while (1)
 	{
-		if (digitResult.Multiply(divNum[curIdx++]))
-			break;
-		if (curIdx == divNum.size())
+		for (int i = initNum.size() - 1; i >= 0; --i)
+			mod += (initNum[i] % N) * (PowerModular(10, initNum.size() - 1 - i, N) % N);
+		if (mod == 0)
 		{
-			for (int i = 0; i < divNum.size(); ++i)
+			for (int j = 0; j < initNum.size(); ++j)
 			{
-				divNum[i] += 10;
+				cout << initNum[j];
 			}
-			curIdx = divNum[0];
+			cout << '\n';
+			return;
+		}
+		else
+		{
+			if (totalCount1 < totalNumCount)
+			{
+				if (initNum.back() == 0)
+				{
+					initNum.pop_back();
+					initNum.push_back('1');
+					++totalCount1;
+				}
+				else
+				{
+
+				}
+
+			}
 		}
 	}
 
-
-	if (result == -1)
-		cout << "BRAK" << '\n';
 }
 
 int main()
