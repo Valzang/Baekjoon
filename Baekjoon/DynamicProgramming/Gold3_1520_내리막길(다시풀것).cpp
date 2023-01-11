ba623dabfa12535252dedb782303fd89278d9b9a
JCW <72681774+Valzang@users.noreply.github.com>
2023-01-11 11:33:59 +0900

    동물원

--- Baekjoon/DynamicProgramming/Gold3_1520_내리막길(다시풀것).cpp
+++ Baekjoon/DynamicProgramming/Gold3_1520_내리막길(다시풀것).cpp
@@ -3,44 +3,49 @@
 
 using namespace std;
 
+int N, M;
 int heights[501][501];
+int passed[501][501];
+
+int DFS(int row, int col)
+{
+	if (passed[row][col] != -1)
+		return passed[row][col];
+
+	if (row == M && col == N)
+		return 1;
+
+	passed[row][col] = 0;
+
+	int curNum = heights[row][col];
+
+	if (row > 1 && heights[row - 1][col] < curNum)
+		passed[row][col] += DFS(row - 1, col);
+	if (row < M && heights[row + 1][col] < curNum)
+		passed[row][col] += DFS(row + 1, col);
+	if (col > 1 && heights[row][col - 1] < curNum)
+		passed[row][col] += DFS(row, col - 1);
+	if (col < N && heights[row][col + 1] < curNum)
+		passed[row][col] += DFS(row, col + 1);
+
+	return passed[row][col];
+
+}
 
 int main()
 {
-	int N, M;
-	int result = 0;
-	stack<pair<int, int>> ways;
-
 	cin >> M >> N;
+
 	for (int i = 1; i <= M; ++i)
 	{
 		for (int j = 1; j <= N; ++j)
+		{
 			cin >> heights[i][j];
+			passed[i][j] = -1;
+		}
 	}
-
-	ways.push(make_pair(1, 1));
-	while (!ways.empty())
-	{
-		int row = ways.top().first;
-		int col = ways.top().second;
-
-		if (row == M && col == N)
-			++result;
-
-		int curNum = heights[row][col];
-		ways.pop();
-		if (row > 1 && heights[row - 1][col] < curNum)
-			ways.push(make_pair(row - 1, col));
-		if (col > 1 && heights[row][col - 1] < curNum)
-			ways.push(make_pair(row, col - 1));
-		if (row < M && heights[row + 1][col] < curNum)
-			ways.push(make_pair(row + 1, col));
-		if (col < N && heights[row][col + 1] < curNum)
-			ways.push(make_pair(row, col + 1));
-	}
-
-	cout << result;
-
+	
+	cout << DFS(1,1);
 
 	return 0;
 }
--- Baekjoon/DynamicProgramming/Silver1_1309_동물원.cpp
+++ Baekjoon/DynamicProgramming/Silver1_1309_동물원.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -6,9 +7,16 @@ int main()
 {
 	int N;
 	int divider = 9901;
+	vector<int> results;
 
-	// 0인 경우는 무조건 포함.
-	int result = 1;
+	cin >> N;
+	results.resize(N+1);
+	results[0] = 1;
+	results[1] = 3;
+	for (int i = 2; i <= N; ++i)
+		results[i] = (results[i - 1] * 2 % divider + results[i - 2] % divider) % divider;
+
+	cout << results[N];
 
 	return 0;
 }
