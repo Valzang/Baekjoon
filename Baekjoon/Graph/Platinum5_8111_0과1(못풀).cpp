092ff8388dd657346fd9c2afb4c1c1a10ebd7e59
JCW <72681774+Valzang@users.noreply.github.com>
2023-02-07 12:30:46 +0900

    연속합

--- Baekjoon/Graph/Platinum5_8111_0과1(못풀).cpp
+++ Baekjoon/Graph/Platinum5_8111_0과1(못풀).cpp
@@ -25,95 +25,12 @@ using namespace std;
 
 */
 
-// string을 점점 키워나가면서 중간에 N으로 나눠떨어지면 출력. string의 크기가 100을 초과하면 BRAK 출력
-// (A+B)%C = ((A%C) + (B%C)) %C
-// (A*B)%C = ((A%C) * (B%C)) %C
-// (A*A)%C = ((A%C) * (A%C)) %C
-//	=> (10^99)%C = ((10^9 % C)^10)%C
-// ex) 123456 % N =>
-//		(1* 10^5 + 2*10^4 + 3*10^3 + 4*10^2 + 5*10^1 + 6*10^0) % N
-//		=> ((1* 10^5 % N) + (2*10^4 % N ) + ( 3*10^3 % N ) + (4*10^2 % N ) + (5*10^1 %N ) + (6*10^0 % N )) %N
-
 // int max => 2,147,483,647		총 10자리 ( 10^9)
-// (10^9)^9
-// 10^10 = 10^9 * 10
-// 10^99 = (10^9)^9
-
-int PowerModular(int num, int pow, int mod)
-{
-	int answer = 1;
-	while (pow)
-	{
-		if (pow % 2 == 1)
-			answer = (answer * num % mod);
-		num = num * num % mod;
-		pow /= 2;
-	}
-	return answer;
-}
-
-
-void Solution(int N)
-{
-	string stringN = to_string(N);
-	string initNum = "1";	
-	// N의 자릿수와 동일하게 맞춰주기
-	for (int i = 1; i < stringN.size(); ++i)
-		initNum.append("0");	
-
-	int totalCount1 = 1;
-	int curIndex = 0;
-	int totalNumCount = initNum.size();
-
-	int mod = 0;
-	while (1)
-	{
-		for (int i = initNum.size() - 1; i >= 0; --i)
-			mod += (initNum[i] % N) * (PowerModular(10, initNum.size() - 1 - i, N) % N);
-		if (mod == 0)
-		{
-			for (int j = 0; j < initNum.size(); ++j)
-			{
-				cout << initNum[j];
-			}
-			cout << '\n';
-			return;
-		}
-		else
-		{
-			if (totalCount1 < totalNumCount)
-			{
-				if (initNum.back() == 0)
-				{
-					initNum.pop_back();
-					initNum.push_back('1');
-					++totalCount1;
-				}
-				else
-				{
-
-				}
-
-			}
-		}
-	}
-
-}
 
 int main()
 {
 	int T = 0;
 
-	cin >> T;
-	int* N = new int[T];
-	memset(N, 0, sizeof(int) * T);
-
-	for (int i = 0; i < T; ++i)
-	{
-		cin >> N[i];
-	}
-
-	delete[] N;
 
 	return 0;
 }
